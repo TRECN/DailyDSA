7e84ea7edf9c05f393da13f15c1cd64578d92908
TRECN <rishabh1103@outlook.com>
2022-08-31 10:26:45 +0530

    commit

--- Array/ArrayRotation/arrayRotation.cpp
+++ Array/ArrayRotation/arrayRotation.cpp
@@ -20,11 +20,18 @@ void arrayRotation(int arr[],int d, int n){
 
 }
 
+void printArr(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}
+
 int main()
 {
     int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
     int N = sizeof(arr) / sizeof(arr[0]);
     int d = 2;
- 
+    arrayRotation(arr,d,N);
+    printArr(arr,N);
     return 0;
 }
