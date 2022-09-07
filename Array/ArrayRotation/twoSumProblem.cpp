283169353701ac76e7d2a996ef4af6a20a950836
TRECN <rishabh1103@outlook.com>
2022-09-08 01:03:58 +0530

    commit

--- Array/ArrayRotation/twoSumProblem.cpp
+++ Array/ArrayRotation/twoSumProblem.cpp
@@ -4,7 +4,7 @@ using namespace std;
 vector<int> twoSum(vector<int>&nums,int target){
     vector<int>ans;
     unordered_map <int,int>mp;
-    for(int i=0;i<nums.size();++){
+    for(int i=0;i<nums.size();i++){
         if(mp.find(target-nums[i])!=mp.end()){
             ans.push_back(mp[target-nums[i]]);
             ans.push_back(i);
@@ -17,5 +17,13 @@ vector<int> twoSum(vector<int>&nums,int target){
 }
 
 int main(){
+    vector<int>nums;
+    nums.push_back(2);
+    nums.push_back(7);
+    nums.push_back(11);
+    nums.push_back(15);
 
+    vector<int>sum(twoSum(nums,9));
+    cout<<"{"<<sum[0]<<","<<sum[1]<<"}";
 }
+// 2,7,11,15
