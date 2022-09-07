1e957f2467034ebd5211b485909ecd6d44b797ce
TRECN <rishabh1103@outlook.com>
2022-09-07 19:56:04 +0530

    commit

---
+++ Array/ArrayRotation/stlImplimentation.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>vect={5,3,2,6,1,8,9,0,7};
+    reverse(vect.begin(),vect.end());
+    for(int i=0;i<vect.size();i++){
+        cout<<vect[i]<<" ";
+    }
+    return 0;
+}
