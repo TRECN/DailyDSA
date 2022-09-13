75dafeb5fb86c9be5829ee85b7fd930633c99526
TRECN <rishabh1103@outlook.com>
2022-09-13 20:37:17 +0530

    commit

--- Array/Sub Array/MaxSubarrSumKConcat.cpp
+++ Array/Sub Array/MaxSubarrSumKConcat.cpp
@@ -1,12 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+long long maxSubSumKConcat(vector<int> &arr, int n, int k)
+{
+    long long maxSum = -1e15;
+    long long curSum = 0; 
+  
+    for (int i = 0; i < n * k; i++) 
+    { 
+        curSum += arr[i % n]; 
+  
+        maxSum = max(maxSum, curSum);
+  
+        if (curSum < 0)
+        {
+            curSum = 0;
+        } 
+    } 
+
+    return maxSum; 
+}
 int main(){
     vector<int>v={23 ,44 ,6, 12};
     vector<int>a;
-    int k=2;
-    while(k--)
-        a.insert(a.end(),v.begin(),v.end());
-
-    for(auto i:a)
-        cout<<i<<" ";
+    
 }
