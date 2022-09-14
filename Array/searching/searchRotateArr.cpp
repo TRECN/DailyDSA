30658b68a29e4677c8da2efe0cd69529df60a51c
TRECN <rishabh1103@outlook.com>
2022-09-14 19:22:12 +0530

    commit

---
+++ Array/searching/searchRotateArr.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int searchRotateArr(int *a,int n,int k){
+    int low=0;
+    int high=n-1;
+    while(low<=high){
+        int mid=(low+high)>>1;
+        if(a[mid]==k)
+            return mid;
+        else if(a[low]<=a[mid]){
+            if(a[low]<=k&&k<=a[high])
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        else{
+            if(k>=a[mid]&&k<=a[high])
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+    }
+    return -1;
+}
+int main(){
+    int a[]={4,5,6,7,0,1,2};
+    cout<<searchRotateArr(a,7,1);
+    return 0;
+}
