de8a8b2d028462750fc43cc0e8dcb8848df014df
TRECN <rishabh1103@outlook.com>
2022-09-22 16:00:19 +0530

    commit

---
+++ Array/searching/cla.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a=20;
+class student{
+	
+    public:
+    int a=10;
+    student(int a){
+    	cout<<a;
+	}
+    
+};
+
+
+int main(){
+    student *s=new student(1);
+    cout<<s->a;
+}
+//1 
+//10
+
+//101
+
+//110
+
+//10 
+//1
+
+
+
+// local variable 
+// ->variable name
+
+// member variable k-
