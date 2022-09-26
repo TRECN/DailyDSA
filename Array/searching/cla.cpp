1830923fd5124c4f357333d8a632e1771cc4e3be
TRECN <rishabh1103@outlook.com>
2022-09-26 22:06:15 +0530

    commit

--- Array/searching/cla.cpp
+++ Array/searching/cla.cpp
@@ -1,34 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-int a=20;
-class student{
-	
-    public:
-    int a=10;
-    student(int a){
-    	cout<<a;
-	}
-    
-};
-
-
 int main(){
-    student *s=new student(1);
-    cout<<s->a;
+    cout<<pow(9,(1/3));
 }
-//1 
-//10
-
-//101
-
-//110
-
-//10 
-//1
-
-
-
-// local variable 
-// ->variable name
-
-// member variable k-
