3c197b03c863f318be1f47350372bf23fbecdb37
TRECN <rishabh1103@outlook.com>
2022-09-27 21:00:53 +0530

    commit

---
+++ Recursion/sortStackRec.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void insertE(stack<int>&s,int temp){
+    if(s.empty()||s.top()<=temp){
+        s.push(temp);
+        return;
+    }
+    int x=s.top();
+    s.pop();
+    insertE(s,temp);
+    s.push(x);
+}
+void sortt(stack<int>&s){
+    if(s.empty())
+        return;
+    int temp=s.top();
+    s.pop();
+    sortt(s);
+    insertE(s,temp);
+}
+int main(){
+    stack<int>s;
+    s.push(5);
+    s.push(3);
+    s.push(4);
+    s.push(1);
+    s.push(2);
+    sortt(s);
+};
