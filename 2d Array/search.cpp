c1ad66c87a565a3a1118d20e569910ce741b5678
TRECN <rishabh1103@outlook.com>
2022-10-02 20:48:39 +0530

    commit

---
+++ 2d Array/search.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+int sc(vector<int>&a,int n,int t){
+    int l=0;
+    int h=n-1;
+    while(l<=h){
+        int mid=(l+h)>>1;
+        if(t==a[mid])
+            return 1;
+        else if(a[mid]>t){
+            h--;
+        }
+        else
+            l++;
+    }
+    return 0;
+}
+bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target) {
+    int i=0;
+    for(auto a:mat){
+        i=sc(a,n,target); 
+        if(i==1)
+            break;
+    }
+    return (i)?true:false;
+}
