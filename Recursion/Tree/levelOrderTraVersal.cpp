18e779e496446ae7524c04418f88e420ceca465c
TRECN <rishabh1103@outlook.com>
2022-10-06 20:51:58 +0530

    commit

--- Recursion/Tree/levelOrderTraVersal.cpp
+++ Recursion/Tree/levelOrderTraVersal.cpp
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h> 
 /************************************************************
 
+    Following is the BinaryTreeNode class structure
+
+    template <typename T>
+    class BinaryTreeNode {
+       public:
+        T val;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
 
 ************************************************************/
 vector<int> getLevelOrder(BinaryTreeNode<int> *root)
