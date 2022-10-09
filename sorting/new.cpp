b5e4941d4b58a7bd3384179c02dfbcc3c3241be9
TRECN <rishabh1103@outlook.com>
2022-10-09 19:13:21 +0530

    commit

---
+++ sorting/new.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int var=2;
+    switch(var)
+    {
+    case 1:
+    cout<<1;
+    case 2:
+    cout<<2;
+    case 3:
+    cout<<3;
+    default:
+    cout<<"default";
+    break;
+
+    }
+
+    return 0;
+}
