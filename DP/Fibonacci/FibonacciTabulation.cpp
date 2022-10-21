215dd57351763c64a571a9621b9c7577c144e3c4
TRECN <rishabh1103@outlook.com>
2022-10-21 23:47:27 +0530

    commit

---
+++ DP/Fibonacci/FibonacciTabulation.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fib(int n,vector<int>&t){
+    t[0]=0;
+    t[1]=1;
+    for(int i=2;i<=n;i++){
+        t[i]=t[i-1]+t[i-2];
+    }
+    return t[n];
+}
+int main()
+{   
+    vector<int>t(5+1,-1);
+    cout<<fib(5,t);
+    return 0;
+    
+}
