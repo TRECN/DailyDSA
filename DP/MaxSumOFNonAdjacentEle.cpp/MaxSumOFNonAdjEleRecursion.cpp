4249f9ec5573391aa7cc62c90e9ab6a940291aa7
TRECN <rishabh1103@outlook.com>
2022-10-23 23:59:57 +0530

    commit

--- DP/MaxSumOFNonAdjacentEle.cpp/MaxSumOFNonAdjEleRecursion.cpp
+++ DP/MaxSumOFNonAdjacentEle.cpp/MaxSumOFNonAdjEleRecursion.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 int maxSum(vector<int>&a,int n){
     if(n<=0)return 0;
-    if(n==1) a[0];
-    
+    if(n==1) return a[0];
     else return max(a[n-1]+maxSum(a,n-2),maxSum(a,n-1));
 }
 int maximumNonAdjacentSum(vector<int> &nums){
