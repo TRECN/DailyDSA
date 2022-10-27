ae65b58bf05673216deaf33ea06833a097d8096c
TRECN <rishabh1103@outlook.com>
2022-10-27 16:37:28 +0530

    commit

--- DP/CoinChange/CoinChangeTablulation.cpp
+++ DP/CoinChange/CoinChangeTablulation.cpp
@@ -1,20 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    vector<int> coins={1,2,3};
-    int n=coins.size();
+    vector<int> a={1,2,3};
+    int n=a.size();
     int sum=4;
     vector<vector<int>>t(n+1,vector<int>(sum+1,0));
     for(int i=1;i<n+1;i++){
         for(int j=0;j<sum+1;j++){
             if(j==0&&i>0)
-                return 1;
+                t[i][j]=1;
             else if(a[i-1]<=j)
                 t[i][j]=t[i][j-a[i-1]]+t[i-1][j];
             else 
                 t[i][j]=t[i-1][j];
         }
     }
-    return t[n][sum];
+    cout<<t[n][sum];
     
 }
